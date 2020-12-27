b1722c2527254a1329e78c6f4261eab2901f5ef4
rebiss <barsegyanat.93@gmail.com>
2020-12-27 17:17:14 +0400

    add: gRPC micro

--- rpc-app/src/app.controller.ts
+++ rpc-app/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    private logger = new Logger('AppController');
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @MessagePattern('add')
+    async accumulation(data: number[]) {
+        this.logger.log(`Adding ${data.toString()}`);
+        return this.appService.add(data);
+    }
 }
