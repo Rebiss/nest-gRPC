6ef080d67039243bb229d9cada051de903e09880
rebiss <barsegyanat.93@gmail.com>
2020-12-27 17:55:00 +0400

    add: gRPC server app

--- rpc-app/src/app.service.ts
+++ rpc-app/src/app.service.ts
@@ -1,18 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Logger } from '@nestjs/common';
-
-const logger = new Logger('AppService');
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Hello World!';
+        return 'Hello Nest.js!';
     }
 
-    public postAcumulete(data: number[]): number {
+    public add(data: number[]): number {
         return (data || []).reduce((a: number, b: number) => a + b);
-    }
-
-    public add(data: number[]) {
-        return this.client.send<number, number[]>('add', data);
     }
 }
---
+++ rpc-app/src/interface/app.interface.ts
@@ -0,0 +1,7 @@
+export interface INumberArray {
+    data: number[];
+}
+
+export interface ISumOfNumberArray {
+    sum: number;
+}
